d118c3b71b3e188df667260c9bf9fbd2b069190f
Nadidee <116063902+Nadidee@users.noreply.github.com>
2022-10-25 23:34:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var numbershow = document.getElementById(`telle`)
+var number = 55; 
+
+function minus(){
+    number --;
+    numbershow.innerHTML = number; 
+}
+
+function plus(){
+    number ++; 
+    numbershow.innerHTML = number; 
+}
